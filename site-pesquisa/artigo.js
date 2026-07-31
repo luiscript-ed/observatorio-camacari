702e2e720e89fd6081c3e256ad07ae90c72a5461
luiscript-ed <duduzinho310310@gmail.com>
2026-07-30 22:28:54 -0300

    Adicionando nova pesquisa, novo layout e nova paleta de cores, além da atualização do footer e do nav

--- site-pesquisa/artigo.js
+++ site-pesquisa/artigo.js
@@ -1,24 +1,13 @@
-// Elementos da barra lateral (Painel IA)
-const painelIA = document.getElementById("painelIA");
-const fecharIA = document.getElementById("fecharBarra");
-
 // Elementos da aba de Importância (Esquerda)
 const buttonAba = document.getElementById("abaEsquerda");
 const fecharAba = document.getElementById("fecharEsq");
 const painelEsq = document.getElementById("painelEsq");
 
-
-// --- Eventos do Painel IA (Direito) ---
-fecharIA.addEventListener("click", () => {
-    painelIA.classList.remove("active");
-});
-
-
 // --- Eventos do Painel Importância da Pesquisa (Esquerdo) ---
 buttonAba.addEventListener("click", () => {
     // Abre a aba esquerda e garante que a aba de IA feche para não encavalar
     painelEsq.classList.add("active");
-    painelIA.classList.remove("active");
+    painelIA.classList.add("active");
 });
 
 fecharAba.addEventListener("click", () => {
